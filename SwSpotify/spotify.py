c90a2977b4c0a94bbd8ae0714a5c6d4ab6d87da9
Mario <marioortizmanero@gmail.com>
2019-10-25 00:02:53 +0200

    Global dbus bus (#7)
    * Implementation of is_playing
    
    * Small fixes, better exceptions WIP
    
    * Fix for last commit
    
    * Platform detection, better error
    
    * macOS tested
    
    * Main file, formatting
    
    * Documentation
    
    * Added dev extra requirement
    
    * Now using exceptions
    
    * Fix, documentation
    
    * Improvements to README
    
    * Style fix
    
    * 120 chars line, import style
    
    * Global dbus
    
    * Fixed comment

--- SwSpotify/spotify.py
+++ SwSpotify/spotify.py
@@ -54,9 +54,10 @@ def get_info_linux():
 
     import dbus
 
-    session_bus = dbus.SessionBus()
+    if not hasattr(get_info_linux, 'session_bus'):
+        get_info_linux.session_bus = dbus.SessionBus()
     try:
-        spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+        spotify_bus = get_info_linux.session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
         spotify_properties = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties")
         metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
     except dbus.exceptions.DBusException:
