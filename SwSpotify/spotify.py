79d62e3e46987aec489263236b7db5fa91dda536
Musab Kılıç <musabkilic@protonmail.com>
2019-12-15 13:30:30 +0300

    Change pre-set list with startswith

--- SwSpotify/spotify.py
+++ SwSpotify/spotify.py
@@ -41,7 +41,7 @@ def get_info_windows():
         track = windows[0]
 
     # The window title is the default one when paused
-    if windows[0] in ('Spotify Premium', 'Spotify Free', 'Spotify'):
+    if windows[0].startswith('Spotify'):
         raise SpotifyPaused
 
     return track, artist
